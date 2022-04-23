51c546d6aeeaab472a7ec68eacfc10b506abb602
José Luis Landabaso Díaz <landabaso@gmail.com>
2022-04-23 07:56:14 +0200

    Clear comments

--- src/fees.js
+++ src/fees.js
@@ -1,16 +1,3 @@
-//One function to sampleFeeRate (maxSatsPerByte = 1000 , samples, log=true)
-//One function to query Esplora and get { "1": 4.223, "2": 4.223, "3": 3.6470000000000002, "4": 3.328, "5": 2.706, "6": 2.706, "7": 2.562, "8": 2.476, "9": 2.476, "10": 2.476, "11": 2.476, "12": 2.476, "13": 2.476, "14": 2.476, "15": 2.476, "16": 2.476, "17": 2.476, "18": 2.327, "19": 2.327, "20": 2.012, "21": 2.012, "22": 2.012, "23": 2.012, "24": 2.012, "25": 2.012, "144": 1, "504": 1, "1008": 1 }
-//One function to pass fee to USD/EUR/Whatever
-//  ->coinmarketcap
-//  which currencies support? Let's see Ledger Live supports:
-//
-//
-//  ->Better in rates.js:
-//  ->These should be hosted on farvault.com/price
-//  https://www.blockchain.com/api/exchange_rates_api
-//  https://blockchain.info/ticker
-//
-
 /**
  * Return an array of precomputed feeRates within a range.
  *
