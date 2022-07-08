f0b417c67df88b537799b5f77dd7f5d9e2459433
José Luis Landabaso Díaz <landabaso@gmail.com>
2022-07-08 12:50:29 +0200

    export as a lib

---
+++ index.js
@@ -0,0 +1,8 @@
+export { default as dataFetchers } from './src/dataFetchers';
+export { default as transactions } from './src/transactions';
+export { default as scripts } from './src/scripts';
+export { default as decodeTx } from './src/decodeTx';
+export { default as HDInterface } from './src/HDInterface';
+export { default as coinselect } from './src/coinselect';
+export { default as fees } from './src/fees';
+export { default as serialization } from './src/serialization';
